c290a4d3a43e373118bd3a8322cc30e13f8ac3de
Defelo <elodef42@gmail.com>
2022-10-07 16:13:28 +0200

    Update get_skills

--- api/services/skills.py
+++ api/services/skills.py
@@ -1,10 +1,12 @@
+from typing import cast
+
 from api.services.internal import InternalService
 
 
 async def get_skills() -> set[str]:
     async with InternalService.SKILLS.client as client:
         response = await client.get("/skills")
-        return set(response.json())
+        return {cast(str, skill["id"]) for skill in response.json()}
 
 
 async def get_completed_skills(user_id: str) -> set[str]:
